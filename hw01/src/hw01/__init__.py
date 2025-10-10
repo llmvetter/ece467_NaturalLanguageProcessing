069433abc07d28971b784843a810c7569664d398
llmvetter <lennart.vetter@yahoo.de>
2025-10-10 13:06:20 -0400

    HW01 - Improves cli for clarity and logging

--- hw01/src/hw01/__init__.py
+++ hw01/src/hw01/__init__.py
@@ -16,26 +16,37 @@ def main() -> None:
     log.info("Settings loaded", settings=settings.model_dump())
 
     # Load dataset
-    log.info("Loading Trainnig Corpus")
-    train_corpus: CorpusLoader = CorpusLoader(
+    log.info("Loading Training Corpus")
+    full_corpus: CorpusLoader = CorpusLoader(
         labels_path=settings.data.train_labels_path,
     )
-    train_corpus.load()
+    full_corpus.load()
 
-    # Split dataset
-    train_data, val_data = train_corpus.split(
-        val_ratio=settings.data.split,
-    )
-    if settings.training.mode == "test":
-        train_data = train_corpus.articles
+    if settings.training.mode == "eval":
+        log.info("Running in eval mode")
+        train_data, eval_data = full_corpus.split(
+            val_ratio=settings.data.split,
+        )
+        log.info("Training data loaded", size=len(train_data))
+        log.info("Validation data loaded", size=len(eval_data))
+
+    elif settings.training.mode == "test":
+        log.info("Running in test mode")
+
+        train_data = full_corpus.articles
+        log.info("Loading separate Test Corpus")
+        test_corpus: CorpusLoader = CorpusLoader(
+            labels_path=settings.data.test_data_path,
+        )
+        test_corpus.load()
+        eval_data = test_corpus.articles
+        log.info("Training data loaded", size=len(train_data))
+        log.info("Test data loaded", size=len(eval_data))
 
     train_tokens: list[str] = [article.tokens for article in train_data]
     train_labels: list[str] = [article.label for article in train_data]
-    val_tokens: list[str] = [article.tokens for article in val_data]
-    val_labels: list[str] = [article.label for article in val_data]
-
-    log.info("Training data loaded", size=len(train_data))
-    log.info("Validation data loaded", size=len(val_data))
+    val_tokens: list[str] = [article.tokens for article in eval_data]
+    val_labels: list[str] = [article.label for article in eval_data]
 
     # Initialize the model
     vectorizer = Vectorizer()
@@ -43,8 +54,7 @@ def main() -> None:
 
     # Fit the vectorizer
     tfidf_matrix = vectorizer.fit(train_tokens)
-    log.info("Vectorizer fitted.", matrix_shape=type(tfidf_matrix))
-    log.info("Vectorizer Vocab Size", vocab=len(vectorizer.vocab))
+    log.info("Vectorizer fitted, Vocab Size", vocab=len(vectorizer.vocab))
 
     # Fit the classifier
     classifier.fit(
@@ -56,48 +66,31 @@ def main() -> None:
     # Predict the val data
     val_vectors = vectorizer.transform(val_tokens)
     predictions = classifier(val_vectors)
-    evaluator = Evaluator(
-        true_labels=val_labels,
-        predictions=predictions,
-    )
 
-    accuracy = evaluator.calculate_accuracy()
-    log.info("Eval Accuracy", metric="accuracy", value=accuracy)
-
-    conf_matrix = evaluator.calculate_confusion_matrix()
-    plot_confusion_matrix(
-        labels=evaluator.label_names,
-        conf_matrix=conf_matrix,
-        accuracy=accuracy,
-    )
-
-    if settings.training.mode == "test":
-        # Load test dataset
-        log.info("Loading Test Corpus")
-        test_corpus: CorpusLoader = CorpusLoader(
-            labels_path=settings.data.test_labels_path,
-        )
-        test_corpus.load()
-        test_data = test_corpus.articles
-        test_tokens: list[str] = [article.tokens for article in test_data]
-        test_labels: list[str] = [article.label for article in test_data]
-        log.info("Test data loaded", size=len(train_data))
-
-        test_vectors = vectorizer.transform(test_tokens)
-        predictions = classifier(test_vectors)
+    if settings.training.mode == "eval":
         evaluator = Evaluator(
-            true_labels=test_labels,
+            true_labels=val_labels,
             predictions=predictions,
         )
         accuracy = evaluator.calculate_accuracy()
-        log.info("Test Accuracy", metric="accuracy", value=accuracy)
+        log.info("Eval Accuracy", metric="accuracy", value=accuracy)
 
+        conf_matrix = evaluator.calculate_confusion_matrix()
+        plot_confusion_matrix(
+            labels=evaluator.label_names,
+            conf_matrix=conf_matrix,
+            accuracy=accuracy,
+        )
+
+    elif settings.training.mode == "test":
+        # Load test dataset
         log.info(
-            "Writing final predictions to file", output_path=settings.data.output_path
+            "Writing final predictions to file",
+            output_path=settings.data.output_path,
         )
 
         output_lines = []
-        for article, predicted_label in zip(test_data, predictions):
+        for article, predicted_label in zip(eval_data, predictions):
             output_line = f"{article.rel_path} {predicted_label}\n"
             output_lines.append(output_line)
 
